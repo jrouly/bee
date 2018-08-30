c5c117f10158224810cca1f13a5af0fadd6dd42d
Jean Michel Rouly <michel.rouly@gmail.com>
2018-08-29 23:58:35 -0400

    remove unused dependencies

--- bees-direct-lambda/src/main/scala/direct/bees/lambda/module/BeeLambdaModule.scala
+++ bees-direct-lambda/src/main/scala/direct/bees/lambda/module/BeeLambdaModule.scala
@@ -1,20 +1,12 @@
 package direct.bees.lambda.module
 
-import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, Materializer}
 import com.softwaremill.macwire.{Module, wire}
 import direct.bees.lambda.{BeeLambda, StandardBeeLambda}
 import direct.bees.module.BeeServiceModule
 import net.rouly.common.config.Configuration
 
-import scala.concurrent.ExecutionContext
-
 @Module
 class BeeLambdaModule {
-
-  implicit val actorSystem: ActorSystem = ActorSystem()
-  implicit val executionContext: ExecutionContext = actorSystem.dispatcher
-  implicit val materializer: Materializer = ActorMaterializer()
 
   lazy val configuration: Configuration = Configuration.default
   lazy val beeServiceModule: BeeServiceModule = wire[BeeServiceModule]
